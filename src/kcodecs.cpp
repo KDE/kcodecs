c7b472a2ec96f0c94a64aaa3356d46e8938c1529
Albert Astals Cid <aacid@kde.org>
2019-02-05 23:26:18 +0100

    Remove unused hexChars global variable

--- src/kcodecs.cpp
+++ src/kcodecs.cpp
@@ -62,11 +62,6 @@
 namespace KCodecs
 {
 
-static const char hexChars[16] = {
-    '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-};
-
 static QList<QByteArray> charsetCache;
 
 
