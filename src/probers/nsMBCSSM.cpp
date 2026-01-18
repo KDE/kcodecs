2c2d3f5acfe822352eaebf304b1326e257c90151
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2026-01-18 22:32:46 +0100

    [KEncodingProber] Remove disabled, obsolete GB2312 data

--- src/probers/nsMBCSSM.cpp
+++ src/probers/nsMBCSSM.cpp
@@ -191,59 +191,6 @@ const SMModel EUCKRSMModel = {
     "EUC-KR",
 };
 
-/* obsolete GB2312 by gb18030
-static unsigned int GB2312_cls [ 256 / 8 ] = {
-//0,1,1,1,1,1,1,1,  // 00 - 07
-1,1,1,1,1,1,1,1,  // 00 - 07
-1,1,1,1,1,1,0,0,  // 08 - 0f
-1,1,1,1,1,1,1,1,  // 10 - 17
-1,1,1,0,1,1,1,1,  // 18 - 1f
-1,1,1,1,1,1,1,1,  // 20 - 27
-1,1,1,1,1,1,1,1,  // 28 - 2f
-1,1,1,1,1,1,1,1,  // 30 - 37
-1,1,1,1,1,1,1,1,  // 38 - 3f
-1,1,1,1,1,1,1,1,  // 40 - 47
-1,1,1,1,1,1,1,1,  // 48 - 4f
-1,1,1,1,1,1,1,1,  // 50 - 57
-1,1,1,1,1,1,1,1,  // 58 - 5f
-1,1,1,1,1,1,1,1,  // 60 - 67
-1,1,1,1,1,1,1,1,  // 68 - 6f
-1,1,1,1,1,1,1,1,  // 70 - 77
-1,1,1,1,1,1,1,1,  // 78 - 7f
-1,0,0,0,0,0,0,0,  // 80 - 87
-0,0,0,0,0,0,0,0,  // 88 - 8f
-0,0,0,0,0,0,0,0,  // 90 - 97
-0,0,0,0,0,0,0,0,  // 98 - 9f
-0,2,2,2,2,2,2,2,  // a0 - a7
-2,2,3,3,3,3,3,3,  // a8 - af
-2,2,2,2,2,2,2,2,  // b0 - b7
-2,2,2,2,2,2,2,2,  // b8 - bf
-2,2,2,2,2,2,2,2,  // c0 - c7
-2,2,2,2,2,2,2,2,  // c8 - cf
-2,2,2,2,2,2,2,2,  // d0 - d7
-2,2,2,2,2,2,2,2,  // d8 - df
-2,2,2,2,2,2,2,2,  // e0 - e7
-2,2,2,2,2,2,2,2,  // e8 - ef
-2,2,2,2,2,2,2,2,  // f0 - f7
-2,2,2,2,2,2,2,0)   // f8 - ff
-};
-
-static unsigned int GB2312_st [ 2] = {
-eError,eStart,     3,eError,eError,eError,eError,eError,//00-07
-eItsMe,eItsMe,eItsMe,eItsMe,eError,eError,eStart,eStart) //08-0f
-};
-
-static const unsigned int GB2312CharLenTable[] = {0, 1, 2, 0};
-
-SMModel GB2312SMModel = {
-  {eIdxSft4bits, eSftMsk4bits, eBitSft4bits, eUnitMsk4bits, GB2312_cls },
-   4,
-  {eIdxSft4bits, eSftMsk4bits, eBitSft4bits, eUnitMsk4bits, GB2312_st },
-  GB2312CharLenTable,
-  "GB2312",
-};
-*/
-
 // the following state machine data was created by perl script in
 // intl/chardet/tools. It should be the same as in PSM detector.
 constexpr std::array<unsigned int, 256 / 8> GB18030_cls = PCKXBITS(
